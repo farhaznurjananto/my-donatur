481674aa410073f82a43e62f384d2a221cd8caf0
farhaznurjananto <farhaznurjananto052@gmail.com>
2022-06-09 13:21:45 +0700

    landingpage style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-// Show Password
+// SHOW PASSWORD
 function togglePassword() {
   const pw = document.getElementById("password");
   if (pw.type === "password") {
     pw.type = "text";
   } else {
     pw.type = "password";
+  }
+}
+
+// NAVBAR RESPONSIVE
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
 }
